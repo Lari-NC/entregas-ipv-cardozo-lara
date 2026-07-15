be8c07679783b050317b1989ed3f275acebf7981
Lara Noeli Cardozo <cardozo.lara.edu@gmail.com>
2026-07-14 22:48:34 -0300

    desafio 2

---
+++ Desafio_2/entrega-2-cardozo-lara-noeli/entities/AbstractProjectile.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+class_name Projectile 
+
+@export
+var speed:float
+
+# Vector normalizado
+var direction:Vector2
+
+func _ready():
+	set_physics_process(false)
+	
+func set_starting_values(starting_position:Vector2, direction:Vector2):
+	global_position = starting_position
+	self.direction = direction
+	set_physics_process(true)
+
+func _physics_process(delta: float) -> void:
+	position += direction*speed*delta
---
+++ Desafio_2/entrega-2-cardozo-lara-noeli/entities/player/Cannon.gd
@@ -0,0 +1,23 @@
+extends Sprite2D
+
+@onready
+var fire_position:Marker2D = $FirePosition
+
+#var projectile_scene:PackedScene = preload('res://entities/player/Projectile.tscn')
+@export
+var projectile_scene:PackedScene 
+
+var projectile_container:Node
+
+func fire():
+	var projectile_instance:Projectile = projectile_scene.instantiate()
+	projectile_container.add_child(projectile_instance)
+	#projectile_instance.set_starting_values(
+		#fire_position.global_position, 
+		#(fire_position.global_position - global_position).normalized()
+	#)
+	projectile_instance.set_starting_values(
+		fire_position.global_position, 
+		global_transform.x
+	)
+	
